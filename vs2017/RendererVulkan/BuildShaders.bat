50486fd926b4dc20cb32cce15dbf845679d9c06f
Guilherme Lampert <guilherme.ronaldo.lampert@gmail.com>
2020-09-28 00:27:09 +0100

    Add batch file to build vulkan shaders (SPIR-V).
    Using DXC from the VK SDK path.

---
+++ vs2017/RendererVulkan/BuildShaders.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set DXC=%VK_SDK_PATH%\Bin\dxc.exe
+
+:loop
+	set inFile=%1
+	shift /1
+	set outFile=%1
+	shift /1
+
+	:: Compile both VS and PS
+	%DXC% -spirv -T vs_6_0 -O3 -E VS_main -Fo %outFile%.vs %inFile%
+	%DXC% -spirv -T ps_6_0 -O3 -E PS_main -Fo %outFile%.ps %inFile%
+
+	:: Check for further batch arguments or break the loop
+	IF [%1]==[] (
+		goto end
+	) ELSE (
+		goto loop
+	)
+:end
